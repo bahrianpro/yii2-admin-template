a913033d020b60638da61f9fe016ab91c42e9275
Skorobogatko Alexei <skorobogatko.oleksii@gmail.com>
2016-03-15 10:51:02 +0200

    Migration base class.

--- app/base/Migration.php
+++ app/base/Migration.php
@@ -16,19 +16,30 @@ class Migration extends \yii\db\Migration
 {
     
     /**
-     * @var string
+     * @var string default database table options.
      */
     protected $tableOptions = null;
     
     /**
+     * Is MS SQL Server database driver used ?
      * @return bool
      */
     protected function isMSSQL()
     {
         return $this->db->driverName === 'mssql' || $this->db->driverName === 'sqlsrv' || $this->db->driverName === 'dblib';
     }
+    
+    /**
+     * Is SQLite database driver used ?
+     * @return bool
+     */
+    protected function isSqlite()
+    {
+        return $this->db->driverName === 'sqlite';
+    }
 
     /**
+     * Get specific table options for database driver.
      * @return string
      */
     protected function getTableOptions()
@@ -37,7 +48,7 @@ class Migration extends \yii\db\Migration
             switch ($this->db->driverName) {
                 case 'mysql':
                     // http://stackoverflow.com/questions/766809/whats-the-difference-between-utf8-general-ci-and-utf8-unicode-ci
-                    $this->tableOptions = 'CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE=InnoDB';
+                    $this->tableOptions = 'CHARACTER SET utf8 COLLATE utf8_unicode_ci';
                     break;
                 
                 default:
@@ -46,6 +57,14 @@ class Migration extends \yii\db\Migration
         }
         
         return $this->tableOptions;
+    }
+    
+    /**
+     * @inheritdoc
+     */
+    public function createTable($table, $columns, $options = null)
+    {
+        parent::createTable($table, $columns, $options === null ? $this->getTableOptions() : $options);
     }
     
 }
