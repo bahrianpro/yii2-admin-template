c8e84781cdcd95f260023d5f3130d3c5535fd0b3
Skorobogatko Alexei <skorobogatko.oleksii@gmail.com>
2016-03-18 10:07:41 +0200

    Import password reset view.

---
+++ app/views/user/passwordReset.php
@@ -0,0 +1,50 @@
+<?php
+use yii\helpers\Html;
+use yii\helpers\Url;
+use yii\bootstrap\ActiveForm;
+
+/* @var $this yii\web\View */
+/* @var $form yii\bootstrap\ActiveForm */
+/* @var $model app\forms\PasswordReset */
+
+$this->title = Yii::t('app', 'Reset password');
+
+$fieldOptions = function ($icon) {
+    return [
+        'options' => ['class' => 'form-group has-feedback'],
+        'inputTemplate' => "{input}<span class='glyphicon glyphicon-$icon form-control-feedback'></span>"
+    ];
+};
+?>
+<div class="login-box">
+    <div class="login-logo">
+        <a href="<?= Url::home(true) ?>"><b><?= Yii::$app->name ?></b></a>
+    </div>
+    <!-- /.login-logo -->
+    <div class="login-box-body">
+        <p class="login-box-msg"><?= Yii::t('app', 'Please, change your password.') ?></p>
+
+        <?php $form = ActiveForm::begin(['id' => 'reset-password-form', 'enableClientValidation' => false]); ?>
+
+        <?= $form
+            ->field($model, 'password', $fieldOptions('lock'))
+            ->label(false)
+            ->passwordInput(['placeholder' => $model->getAttributeLabel('password')]) ?>
+
+        <?= $form
+            ->field($model, 'password_repeat', $fieldOptions('lock'))
+            ->label(false)
+            ->passwordInput(['placeholder' => $model->getAttributeLabel('password_repeat')]) ?>
+
+        <div class="row">
+            <div class="col-xs-4">
+                <?= Html::submitButton(Yii::t('app', 'Submit'), ['class' => 'btn btn-primary btn-block btn-flat', 'name' => 'submit-button']) ?>
+            </div>
+            <!-- /.col -->
+        </div>
+
+        <?php ActiveForm::end(); ?>
+
+    </div>
+    <!-- /.login-box-body -->
+</div><!-- /.login-box -->
