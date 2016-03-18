785309e832bf5e30b757e52551761bf1056e5448
Skorobogatko Alexei <skorobogatko.oleksii@gmail.com>
2016-03-18 18:09:16 +0200

    Profile.

--- app/views/user/profile.php
+++ app/views/user/profile.php
@@ -10,4 +10,39 @@ $this->title = Yii::t('app', 'User Profile');
 $this->params['breadcrumbs'] = [
     ['label' => 'User Profile'],
 ];
+
+// TODO: used for demo.
+$directoryAsset = Yii::$app->assetManager->getPublishedUrl('@vendor/almasaeed2010/adminlte/dist');
 ?>
+
+<div class="row">
+    <div class="col-md-3">
+        <!-- Profile image -->
+        <div class="box box-primary">
+            <div class="box-body box-profile">
+                <img class="profile-user-img img-responsive img-circle" src="<?= $directoryAsset ?>/img/user2-160x160.jpg">
+                <h3 class="profile-username text-center">
+                    <?= Html::encode($model->name) ?>
+                </h3>
+            </div> <!-- /.box-body -->
+        </div> <!-- /.box -->
+    </div>
+    
+    <div class="col-md-9">
+    <?php $form = ActiveForm::begin([
+        'id' => 'user-profile-form',
+        'layout' => 'horizontal',
+        'enableClientValidation' => true,
+    ]) ?>
+
+        <?= $form->field($model, 'name') ?>
+
+        <?= $form->field($model, 'password') ?>
+
+        <?= $form->field($model, 'password_repeat') ?>
+
+    <?php ActiveForm::end() ?>        
+    </div>
+    
+</div>
+
