a5906655c5bce6aec8c459dc7618f623b68f3e46
Skorobogatko Alexei <skorobogatko.oleksii@gmail.com>
2016-03-21 10:12:51 +0200

    No longer used.

--- app/views/user/profile.php
+++ app/views/user/profile.php
@@ -1,19 +1,14 @@
 <?php
 use yii\helpers\Html;
-use yii\bootstrap\ActiveForm;
 use yii\bootstrap\Tabs;
 
 /* @var $this yii\web\View */
-/* @var $form yii\bootstrap\ActiveForm */
 /* @var $model app\forms\Profile */
 
 $this->title = Yii::t('app', 'User Profile');
 $this->params['breadcrumbs'] = [
     ['label' => 'User Profile'],
 ];
-
-// TODO: used for demo.
-$directoryAsset = Yii::$app->assetManager->getPublishedUrl('@vendor/almasaeed2010/adminlte/dist');
 ?>
 
 <div class="row">
