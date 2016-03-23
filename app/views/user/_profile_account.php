00da1de70284847e98440e9f37e752ac4573aba2
Skorobogatko Alexei <skorobogatko.oleksii@gmail.com>
2016-03-23 16:59:30 +0200

    Flat button

--- app/views/user/_profile_account.php
+++ app/views/user/_profile_account.php
@@ -28,7 +28,7 @@ use yii\helpers\Html;
 
     <div class="form-group">
         <div class="col-sm-offset-2 col-sm-10">
-            <?= Html::submitButton(Yii::t('app', 'Submit'), ['class' => 'btn btn-danger']) ?>
+            <?= Html::submitButton(Yii::t('app', 'Submit'), ['class' => 'btn btn-danger btn-flat']) ?>
         </div>
     </div>
 
