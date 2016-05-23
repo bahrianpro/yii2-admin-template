1615370f28e662f570303bc051d116317ef21bf0
Skorobogatko Alexei <skorobogatko.oleksii@gmail.com>
2016-05-23 15:17:31 +0300

    Shift user info to right side.

--- app/views/user/profile.php
+++ app/views/user/profile.php
@@ -12,20 +12,6 @@ $this->params['breadcrumbs'] = [
 ?>
 
 <div class="row">
-    <div class="col-md-3">
-        <!-- Profile image -->
-        <div class="box box-primary">
-            <div class="box-body box-profile">
-                <?= Html::img(Yii::$app->params['noAvatarImage'], ['class' => 'profile-user-img img-responsive img-circle']) ?>
-                <h3 class="profile-username text-center">
-                    <?= Html::encode($model->name) ?>
-                </h3>
-                <p class="text-muted text-center">
-                    <?= Yii::t('app', 'Member since {date}', ['date' => Yii::$app->formatter->asDate(Yii::$app->user->identity->created_at)]) ?>
-                </p>
-            </div> <!-- /.box-body -->
-        </div> <!-- /.box -->
-    </div>
     
     <div class="col-md-9">
         <div class="nav-tabs-custom">
@@ -39,6 +25,21 @@ $this->params['breadcrumbs'] = [
                 ],
             ]) ?>
         </div>
+    </div>
+    
+    <div class="col-md-3">
+        <!-- Profile image -->
+        <div class="box box-primary">
+            <div class="box-body box-profile">
+                <?= Html::img(Yii::$app->params['noAvatarImage'], ['class' => 'profile-user-img img-responsive img-circle']) ?>
+                <h3 class="profile-username text-center">
+                    <?= Html::encode($model->name) ?>
+                </h3>
+                <p class="text-muted text-center">
+                    <?= Yii::t('app', 'Member since {date}', ['date' => Yii::$app->formatter->asDate(Yii::$app->user->identity->created_at)]) ?>
+                </p>
+            </div> <!-- /.box-body -->
+        </div> <!-- /.box -->
     </div>
     
 </div>
