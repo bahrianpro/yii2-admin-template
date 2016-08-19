836446d457ef45977c3aa07f7c9f63c72e2b5ec1
Skorobogatko Alexei <skorobogatko.oleksii@gmail.com>
2016-08-19 10:38:26 +0300

    Parameter overrides.

--- app/components/Param.php
+++ app/components/Param.php
@@ -10,6 +10,7 @@ namespace app\components;
 
 use app\models\Config;
 use ErrorException;
+use Yii;
 use yii\base\Component;
 use yii\base\InvalidValueException;
 use yii\db\Query;
@@ -17,6 +18,13 @@ use yii\db\Query;
 /**
  * Site parameters management.
  *
+ * Example:
+ * ```php
+ * Param::value('Site.param');
+ * ```
+ * will request parameter Site.param in config.web `params` array and
+ * then in `config` database table.
+ * 
  * @author skoro
  */
 class Param extends Component
@@ -37,14 +45,20 @@ class Param extends Component
     
     /**
      * Get parameter value.
-     * @param string $param parameter name without prefixed section returns
-     * all parameters matched by name. Parameter name with section (Site.adminEmail)
-     * return only single parameter.
-     * @param mixed $default
+     * 
+     * First checks config/web.php parameters then checks cached parameters and
+     * finally queries config data from database.
+     *
+     * @param string $param parameter name including section.
+     * @param mixed $default default value if parameter not found.
      * @return mixed
      */
     public static function value($param, $default = null)
     {
+        if (($value = static::getOverrides($param)) !== null) {
+            return $value;
+        }
+        
         if (isset(static::$cache[$param])) {
             return static::$cache[$param]->value;
         }
@@ -101,6 +115,30 @@ class Param extends Component
             'name' => $name,
             'section' => $section,
         ]);
+    }
+    
+    /**
+     * Get config overrides from config/web.php
+     * 
+     * Checked in following order:
+     * Site.param => $params['Site.param']
+     * Site.param => $params['Site']['param']
+     * 
+     * @param string $param parameter name including section.
+     * @return mixed
+     */
+    public static function getOverrides($param)
+    {
+        if (isset(Yii::$app->params[$param])) {
+            return Yii::$app->params[$param];
+        }
+        
+        list ($section, $name) = static::parseParamName($param);
+        if (isset(Yii::$app->params[$section][$name])) {
+            return Yii::$app->params[$section][$name];
+        }
+        
+        return null;
     }
     
     /**
