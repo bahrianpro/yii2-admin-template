6de27cde824fa8262c93ea456fb341aa433bca78
Skorobogatko Alexei <skorobogatko.oleksii@gmail.com>
2016-08-26 11:57:07 +0300

    Allow to copy email to clipboard.

--- app/views/user/_profile_account.php
+++ app/views/user/_profile_account.php
@@ -1,11 +1,17 @@
 <?php
 
+use app\helpers\Icon;
 use app\widgets\ActiveForm;
+use app\widgets\InputGroup;
 use app\widgets\Pjax;
+use yii\bootstrap\Button;
 
 /** @var $this yii\web\View */
 /** @var $form app\widgets\ActiveForm */
 /** @var $model app\forms\user\Profile */
+/** @var $emailId string */
+
+$emailId = \yii\helpers\Html::getInputId($model, 'email');
 ?>
 <?php Pjax::begin() ?>
     <?php $form = ActiveForm::begin([
@@ -21,7 +27,26 @@ use app\widgets\Pjax;
         ],
     ]) ?>
 
-        <?= $form->field($model, 'email')->textInput(['disabled' => 'disabled']) ?>
+        <?= $form->field($model, 'email')->widget(InputGroup::className(), [
+            'inputOptions' => [
+                'class' => 'form-control',
+                'disabled' => 'disabled',
+            ],
+            'button' => true,
+            'addon' => Button::widget([
+                'label' => Icon::icon('glyphicon glyphicon-copy'),
+                'encodeLabel' => false,
+                'options' => ['class' => 'btn btn-default btn-flat'],
+                'clientEvents' => [
+                    'click' => new yii\web\JsExpression("function (ev) {
+                        ev.preventDefault();
+                        $('#{$emailId}').removeAttr('disabled').select();
+                        document.execCommand('copy');
+                        $('#{$emailId}').attr('disabled', 'disabled');
+                    }"),
+                ],
+            ]),
+        ]) ?>
         <?= $form->field($model, 'name') ?>
         <?= $form->field($model, 'password')->passwordInput() ?>
         <?= $form->field($model, 'password_repeat')->passwordInput() ?>
