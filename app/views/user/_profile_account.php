05650553e2f2ae02979278b95b3cd42a6ffd70b7
Skorobogatko Alexei <skorobogatko.oleksii@gmail.com>
2016-08-30 16:48:26 +0300

    It's better to reload page to have all actual changes (changed user name).

--- app/views/user/_profile_account.php
+++ app/views/user/_profile_account.php
@@ -3,55 +3,54 @@
 use app\helpers\Icon;
 use app\widgets\ActiveForm;
 use app\widgets\InputGroup;
-use app\widgets\Pjax;
 use yii\bootstrap\Button;
+use yii\helpers\Html;
+use yii\web\JsExpression;
 
 /** @var $this yii\web\View */
 /** @var $form app\widgets\ActiveForm */
 /** @var $model app\forms\user\Profile */
 /** @var $emailId string */
 
-$emailId = \yii\helpers\Html::getInputId($model, 'email');
+$emailId = Html::getInputId($model, 'email');
 ?>
-<?php Pjax::begin() ?>
-    <?php $form = ActiveForm::begin([
-        'id' => 'user-profile-form',
-        'pjax' => true,
-        'layout' => 'horizontal',
-        'enableClientValidation' => true,
-        'fieldConfig' => [
-            'horizontalCssClasses' => [
-                'label' => 'col-sm-2',
-                'wrapper' => 'col-sm-10',
-            ],
+<?php $form = ActiveForm::begin([
+    'id' => 'user-profile-form',
+    'pjax' => true,
+    'layout' => 'horizontal',
+    'enableClientValidation' => true,
+    'fieldConfig' => [
+        'horizontalCssClasses' => [
+            'label' => 'col-sm-2',
+            'wrapper' => 'col-sm-10',
         ],
-    ]) ?>
+    ],
+]) ?>
 
-        <?= $form->field($model, 'email')->widget(InputGroup::className(), [
-            'inputOptions' => [
-                'class' => 'form-control',
-                'disabled' => 'disabled',
+    <?= $form->field($model, 'email')->widget(InputGroup::className(), [
+        'inputOptions' => [
+            'class' => 'form-control',
+            'disabled' => 'disabled',
+        ],
+        'button' => true,
+        'addon' => Button::widget([
+            'label' => Icon::icon('glyphicon glyphicon-copy'),
+            'encodeLabel' => false,
+            'options' => ['class' => 'btn btn-default btn-flat'],
+            'clientEvents' => [
+                'click' => new JsExpression("function (ev) {
+                    ev.preventDefault();
+                    $('#{$emailId}').removeAttr('disabled').select();
+                    document.execCommand('copy');
+                    $('#{$emailId}').attr('disabled', 'disabled');
+                }"),
             ],
-            'button' => true,
-            'addon' => Button::widget([
-                'label' => Icon::icon('glyphicon glyphicon-copy'),
-                'encodeLabel' => false,
-                'options' => ['class' => 'btn btn-default btn-flat'],
-                'clientEvents' => [
-                    'click' => new yii\web\JsExpression("function (ev) {
-                        ev.preventDefault();
-                        $('#{$emailId}').removeAttr('disabled').select();
-                        document.execCommand('copy');
-                        $('#{$emailId}').attr('disabled', 'disabled');
-                    }"),
-                ],
-            ]),
-        ]) ?>
-        <?= $form->field($model, 'name') ?>
-        <?= $form->field($model, 'password')->passwordInput() ?>
-        <?= $form->field($model, 'password_repeat')->passwordInput() ?>
+        ]),
+    ]) ?>
+    <?= $form->field($model, 'name') ?>
+    <?= $form->field($model, 'password')->passwordInput() ?>
+    <?= $form->field($model, 'password_repeat')->passwordInput() ?>
 
-    <?php ActiveForm::endWithActions([
-        'cancel' => false,
-    ]) ?>        
-<?php Pjax::end() ?>
+<?php ActiveForm::endWithActions([
+    'cancel' => false,
+]) ?>        
