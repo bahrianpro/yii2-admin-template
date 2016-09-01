cc80f0f3fdde3fdeb3955f6ccec5199cdda7e60b
Skorobogatko Alexei <skorobogatko.oleksii@gmail.com>
2016-09-01 12:44:39 +0300

    Module statuses and info.

--- app/base/Module.php
+++ app/base/Module.php
@@ -15,4 +15,24 @@ namespace app\base;
  */
 class Module extends \yii\base\Module
 {
+    /**
+     * Module statuses.
+     */
+    const STATUS_INSTALLED = 'installed';
+    const STATUS_NOTINSTALLED = 'notinstalled';
+    
+    /**
+     * @var string required, module name.
+     */
+    public $moduleName;
+
+    /**
+     * @var string
+     */
+    public $moduleDescription = '';
+    
+    /**
+     * @var string module status.
+     */
+    public $status = self::STATUS_NOTINSTALLED;
 }
