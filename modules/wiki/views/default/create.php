3198f5f4fd69f999c51657460b4f21eeedba0b1b
Skorobogatko Alexei <skorobogatko.oleksii@gmail.com>
2016-09-02 23:36:12 +0300

    Use editor subtemplate.

--- modules/wiki/views/default/create.php
+++ modules/wiki/views/default/create.php
@@ -1,17 +1,7 @@
 <?php
 
-use app\widgets\ActiveForm;
-
 /** @var $this yii\web\View */
 /** @var $editor modules\wiki\forms\Editor */
-/** @var $form app\widgets\ActiveForm */
 ?>
 
-<?php $form = ActiveForm::begin() ?>
-
-    <?= $form->field($editor, 'title') ?>
-    <?= $form->field($editor, 'content')->textArea(['rows' => 16]) ?>
-
-<?php ActiveForm::endWithActions([
-    
-]) ?>
+<?= $this->render('_editor', ['editor' => $editor]) ?>
