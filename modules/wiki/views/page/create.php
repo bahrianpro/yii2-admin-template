5271fc6817ac0c802197bc7c9a96ab6f05e2a65c
Skorobogatko Alexei <skorobogatko.oleksii@gmail.com>
2016-09-05 12:35:40 +0300

    Box wrapper.

--- modules/wiki/views/page/create.php
+++ modules/wiki/views/page/create.php
@@ -1,7 +1,17 @@
 <?php
 
+use app\widgets\Box;
+
 /** @var $this yii\web\View */
 /** @var $editor modules\wiki\forms\Editor */
+
+$this->title = Yii::t('app', 'Create a new page');
+$this->params['breadcrumbs'][] = ['label' => Yii::t('app', 'Wiki'), 'url' => ['default/index']];
+$this->params['breadcrumbs'][] = $this->title;
 ?>
 
+<?php Box::begin([
+    'label' => $this->title,
+]) ?>
 <?= $this->render('_editor', ['editor' => $editor]) ?>
+<?php Box::end() ?>
