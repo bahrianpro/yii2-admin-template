45f7fd5863f7b2c56e139e3132e94b53034260b9
Alexei Skorobogatko <skorobogatko.oleksii@gmail.com>
2018-02-01 10:44:50 +0200

    Like as index.php but config not found yet.

--- web/index-test.php
+++ web/index-test.php
@@ -5,12 +5,15 @@ if (!in_array(@$_SERVER['REMOTE_ADDR'], ['127.0.0.1', '::1'])) {
     die('You are not allowed to access this file.');
 }
 
+define('APPROOT_DIR', dirname(__DIR__));
 defined('YII_DEBUG') or define('YII_DEBUG', true);
 defined('YII_ENV') or define('YII_ENV', 'test');
 
-require(__DIR__ . '/../vendor/autoload.php');
-require(__DIR__ . '/../vendor/yiisoft/yii2/Yii.php');
+require(APPROOT_DIR . '/vendor/autoload.php');
+require(APPROOT_DIR . '/vendor/yiisoft/yii2/Yii.php');
 
-$config = require(__DIR__ . '/../tests/codeception/config/acceptance.php');
+// TODO: where is the config ???
+$config = require(APPROOT_DIR . '/tests/config/acceptance.php');
 
-(new yii\web\Application($config))->run();
+$app = new app\base\WebApplication($config);
+$app->run();
